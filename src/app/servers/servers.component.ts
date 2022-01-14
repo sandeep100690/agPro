59950bd4b669d37f63ba0872b8df205ac5963781
Sandeep Mali <sandeepamali10@gmail.com>
2022-01-14 20:39:45 +0530

    selectors in ag

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+
+  // ################################## attribute selector
+  // selector: '[app-servers]',
+  // ################################## class selector
+  // selector: '.app-servers',
   template: `
     <app-server></app-server>
     <app-server></app-server>
