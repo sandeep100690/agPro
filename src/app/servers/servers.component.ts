6116757fcb507a6d7e64f30f9647217e43eb5e87
Sandeep Mali <sandeepamali10@gmail.com>
2022-02-18 07:13:21 +0530

    b

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -18,6 +18,7 @@ export class ServersComponent implements OnInit {
   allowNewServer : boolean = false;
   serverCreationStatus = 'No server was created';
   serverName = 'Test Server';
+  serverCreated = false;
 
   constructor() { 
     setTimeout(() => {
@@ -29,7 +30,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer(){
-    this.serverCreationStatus = 'Server was created' + this.serverName;
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created, server name is ' + this.serverName;
   }
 
   onUpdateServerName(event:any){
