381444861bb225182bd0a3346ab8c8fc6d2c86de
Sandeep Mali <sandeepamali10@gmail.com>
2022-02-21 06:35:50 +0530

    index in ngFor

--- src/app/demo2/demo2.component.ts
+++ src/app/demo2/demo2.component.ts
@@ -11,7 +11,8 @@ export class Demo2Component implements OnInit {
 
   onToggleDetails(){
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1);
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
   constructor() { }
 
