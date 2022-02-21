e6c1c8ec13f0c2dc6659f8489041accb71df7d9c
Sandeep Mali <sandeepamali10@gmail.com>
2022-02-21 07:15:21 +0530

    new component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,13 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { Demo1Component } from './demo1/demo1.component';
 import { Demo2Component } from './demo2/demo2.component';
+import { HeaderComponent } from './01-basics/header/header.component';
+import { RecipesComponent } from './01-basics/header/recipes/recipes.component';
+import { RecipeDetailComponent } from './01-basics/header/recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './01-basics/header/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './01-basics/header/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './01-basics/header/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './01-basics/header/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +21,14 @@ import { Demo2Component } from './demo2/demo2.component';
     ServerComponent,
     ServersComponent,
     Demo1Component,
-    Demo2Component
+    Demo2Component,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
