b712b8c6f8029507e88ad039fbe4a9c14ad37fff
Sandeep Mali <sandeepamali10@gmail.com>
2022-02-21 23:09:53 +0530

    recipe list added

--- src/app/01-basics/header/recipes/recipe-list/recipe-list.component.ts
+++ src/app/01-basics/header/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../receipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
+
 export class RecipeListComponent implements OnInit {
-  recipes = [];
-  
+  recipes: Recipe[]  = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe-480x270.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe-480x270.jpg')
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
