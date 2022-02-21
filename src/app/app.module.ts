cc36a631636b23cfd85100144c94b2916d65f307
Sandeep Mali <sandeepamali10@gmail.com>
2022-02-21 06:21:35 +0530

    c1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { Demo1Component } from './demo1/demo1.component';
+import { Demo2Component } from './demo2/demo2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    Demo1Component
+    Demo1Component,
+    Demo2Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/demo2/demo2.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo2',
+  templateUrl: './demo2.component.html',
+  styleUrls: ['./demo2.component.scss']
+})
+export class Demo2Component implements OnInit {
+  showSecret:boolean = false;
+  log = [] as any;
+
+  onToggleDetails(){
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
