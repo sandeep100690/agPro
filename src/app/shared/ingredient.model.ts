b3c99aa2566a269ab77bb4936755f5d81b58d4a3
Sandeep Mali <sandeepamali10@gmail.com>
2022-02-27 16:18:45 +0530

    ingredient model created

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,16 @@
+// new syntax
+export class Ingredient{
+    constructor(public name: string, public amount:number){}
+}
+
+
+
+// export class Ingredient{
+//     public name: string;
+//     public amount: number;
+
+//     constructor(name: string, amount:number){
+//         this.name = name;
+//         this.amount = amount;
+//     }
+// }
