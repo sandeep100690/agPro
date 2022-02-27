db2b773b1a492350cac721e3526d70f2d8912494
Sandeep Mali <sandeepamali10@gmail.com>
2022-02-27 16:33:05 +0530

    model implementation in shopp list

--- src/app/01-basics/header/shopping-list/shopping-list.component.ts
+++ src/app/01-basics/header/shopping-list/shopping-list.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss']
 })
+
 export class ShoppingListComponent implements OnInit {
-  ingredients = [];
-  
+  ingredients: Ingredient[] = [] = [
+    new Ingredient('Apples',5),
+    new Ingredient('Tomatoes',25),
+    new Ingredient('Mango',15),
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
